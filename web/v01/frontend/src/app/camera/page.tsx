760b9d2c367adfd7b8216d3b87483a4f0f88f5b6
msanchariii <sancharim2233@gmail.com>
2024-09-28 10:36:31 +0530

    Add ESLint configuration for Next.js and TypeScript

---
+++ web/v01/frontend/src/app/camera/page.tsx
@@ -0,0 +1,13 @@
+import WebcamComponent from "@/components/Webcam";
+import React from "react";
+
+function CameraTest() {
+    return (
+        <div className="">
+            <WebcamComponent />
+            <div></div>
+        </div>
+    );
+}
+
+export default CameraTest;
---
+++ web/v01/frontend/src/components/Webcam.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import Webcam from "react-webcam";
+
+const WebcamComponent = () => <Webcam mirrored={true}></Webcam>;
+
+export default WebcamComponent;
